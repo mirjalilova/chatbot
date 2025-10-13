95ee5b136231dc5bfa2af3becd54350e85375f6e
feruza17 <feruzamirjalilova767@gmail.com>
2025-10-13 14:58:23 +0500

    fixed systax error

--- internal/usecase/repo/chat.go
+++ internal/usecase/repo/chat.go
@@ -121,7 +121,7 @@ func (r *ChatRepo) GetChatRoomChat(ctx context.Context, id *entity.ById, limit, 
 	       created_at
 	FROM chat
 	WHERE chat_room_id = $1 AND deleted_at = 0
-	ORDER BY created_at DASC
+	ORDER BY created_at DESC
 	LIMIT $2 OFFSET $3;
 `
 
